f81bac9d4405f1d9b42e9ab6808dfcdb10be73cc
ecchoi <off.echoi@gmail.com>
2022-08-31 00:28:14 +0900

    fixed untracked files

--- chapter03_hakerNewsClient/app.js
+++ chapter03_hakerNewsClient/app.js
@@ -1,12 +1,15 @@
-const ajax = new XMLHttpRequest()
+const ajax = new XMLHttpRequest();
 
-ajax.open('GET', 'https://api.hnpwa.com/v0/news/1.json', false)
-ajax.send()
+ajax.open('GET', 'https://api.hnpwa.com/v0/news/1.json', false);
+ajax.send();
 
-const newsFeed = JSON.parse(ajax.response)
+const newsFeed = JSON.parse(ajax.response);
+const ul = document.createElement('ul');
 
-document.getElementById('root').innerHTML = `<ul>
-<li>${newsFeed[0].title}</li>
-<li>${newsFeed[0].title}</li>
-<li>${newsFeed[0].title}</li>
-</ul>`
+for (let i = 0; i < newsFeed.length; i++) {
+  const li = document.createElement('li');
+  li.innerHTML = newsFeed[i].title;
+  ul.appendChild(li);
+}
+
+document.getElementById('root').appendChild(ul);
